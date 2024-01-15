01203986268d6b994d0a9953b41a7a803a2f4f62
EnriqueJassoBenito <20213tn033@utez.edu.mx>
2024-01-15 13:29:59 -0600

    aqui se subira los trabajos

---
+++ p2vuelos/src/components/VuelosDisponibles.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>AQUI ES VUELOS DISPONIBLES</h1>
+    <b-breadcrumb :items="items"></b-breadcrumb>
+    <b-link :to= "{name: 'lineasdevuelo'}">Siguiente</b-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          href: "#",
+          to: { name: "lineasdevuelo" },
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
