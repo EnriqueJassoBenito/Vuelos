8a88c1136bdb2f7923c7e3444d51f010ef3e3bf9
EnriqueJassoBenito <20213tn033@utez.edu.mx>
2024-01-29 13:16:55 -0600

    Mejoras en el formulario faltas 3 por completar

--- p2vuelos/src/main.js
+++ p2vuelos/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
@@ -8,6 +9,15 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VeeValidate, {
+  // This is the default
+  inject: true,
+  // Important to name this something other than 'fields'
+  fieldsBagName: 'veeFields',
+  // This is not required but avoids possible naming conflicts
+  errorBagName: 'veeErrors'
+})
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
